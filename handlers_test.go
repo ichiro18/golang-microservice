3d37a4bae93fb61785690b5d75b862ca1cc5df8a
ichiro18 <ichirozcom@gmail.com>
2018-02-28 15:54:34 +0300

    new(service): create microservice

---
+++ handlers_test.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"net/http"
+	"testing"
+	"net/http/httptest"
+	"io/ioutil"
+	"strings"
+	"github.com/takama/router"
+	"bytes"
+	"github.com/sirupsen/logrus"
+	"encoding/json"
+)
+
+var buffer bytes.Buffer
+
+func init()  {
+	log.Out = &buffer
+	log.Formatter = &logrus.JSONFormatter{}
+}
+
+func TestHandler(t *testing.T) {
+	r := router.New()
+	r.GET("/", home)
+
+	ts := httptest.NewServer(r)
+	defer ts.Close()
+
+	res, err := http.Get(ts.URL + "/")
+	if err != nil{
+		t.Fatal(err)
+	}
+
+	greeting, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+
+	if err != nil{
+		t.Fatal(err)
+	}
+
+	expectedGreeting := "Processing URL /..."
+	testingGreeting  := strings.Trim(string(greeting), " \n")
+
+	if expectedGreeting != testingGreeting{
+		t.Fatalf(
+			"Wrong greeting '%s', expected '%s'",
+			testingGreeting, expectedGreeting,
+		)
+	}
+}
+
+func TestLogger(t *testing.T) {
+	r := router.New()
+	r.Logger = logger
+
+	ts := httptest.NewServer(r)
+	defer ts.Close()
+
+	_, err := http.Get(ts.URL  + "/")
+	if err != nil{
+		t.Fatal(err)
+	}
+
+	formatted := struct {
+		Level string `json:"level"`
+		Msg   string `json:"msg"`
+		Time  string `json:"time"`
+	}{}
+
+	err = json.NewDecoder(&buffer).Decode(&formatted)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	msgPart := strings.Split(formatted.Msg, " ")
+	if len(msgPart) != 3{
+		t.Fatalf("Wrong message was logged: %s", formatted.Msg)
+	}
+}
---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/takama/router"
+	"github.com/sirupsen/logrus"
+	"os"
+)
+
+var log = logrus.New()
+
+func main() {
+	port := os.Getenv("SERVICE_PORT")
+	if len(port)  == 0{
+		log.Fatal("Requirement parameter SERVICE_PORT is not set")
+	}
+
+	r:= router.New()
+	r.Logger = logger
+	r.GET("/", home)
+	r.Listen("0.0.0.0:" + port)
+}
